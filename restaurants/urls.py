55f8396172b17c9eb772d32ea732ad127f1dc0b7
Felipe Netto <flpncc@gmail.com>
2018-01-03 20:05:45 -0300

    Adding unique slug generator and slug through url params

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -5,6 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.RestaurantsListView.as_view(), name='restaurants'),
-    path('<int:pk>/', views.RestaurantDetailView.as_view(), name='restaurant_detail'),
-    # path('<str:slug>/', views.RestaurantsListView.as_view(), name='restaurants_search'),
+    path('<slug:slug>/', views.RestaurantDetailView.as_view(), name='restaurant_detail'),
 ]
