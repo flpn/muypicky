58f74607af0223d4989eb4e3e27411e88db742ed
Felipe Netto <flpncc@gmail.com>
2018-01-03 11:19:59 -0300

    Replacing fbv for cbv

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('about/', views.about),
-    path('contacts/', views.contacts),
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+class HomeView(TemplateView):
+    template_name = 'home.html'
 
-
-def about(request):
-    return render(request, 'about.html')    
-
-
-def contacts(request):
-    return render(request, 'contacts.html')
+    def get_context_data(self, **kwargs):
+        context = super(HomeView, self).get_context_data(**kwargs)
+        return context
