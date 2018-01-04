f6ac3f6f3f104717c1478cf30a21fa6abfddf11c
Felipe Netto <flpncc@gmail.com>
2018-01-04 11:44:38 -0300

    Implementing the loginview to access the createview

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('login/', LoginView.as_view(), name='login'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('contacts/', TemplateView.as_view(template_name='contacts.html'), name='contacts'),
     path('restaurants/', include('restaurants.urls')),
